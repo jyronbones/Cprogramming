9746a7d25be4dacba89b27aa14f878a1ad4c204d
Byron <80455013+jyronbones@users.noreply.github.com>
2022-01-25 15:40:35 -0500

    Update 00_Numbers.c

--- Basic Programs/00_Numbers.c
+++ Basic Programs/00_Numbers.c
@@ -3,7 +3,8 @@
 int main()
 {
     /*prints numbers 1-100 and check if it is a multiple of 3 or 5 or both and displaying a message if so */
-    for (int i = 1; i <= 100; i++)
+    int i;
+    for (i = 1; i <= 100; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
             printf("%i I'm a multiple of 3 && 5!!!\n", i);
